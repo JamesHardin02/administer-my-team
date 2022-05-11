7e44bc5916d6def8f564d772ec2feecb1ac3a851
JamesH <jwhusc7@gmail.com>
2022-05-11 16:32:30 -0400

    created role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,26 @@ VALUES
   ('Project Management'),
   ('Marketing'),
   ('HR');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('Visual Designer', 75000, 1),
+  ('UX/UI Designer', 80000, 1),
+  ('Front-End Developer', 80000, 1),
+  ('Front-End Executive Manager', 100000, 1),
+  ('Back-End Developer', 85000, 2),
+  ('Database Developer', 90000, 2),
+  ('Database Administrator', 95000, 2),
+  ('Back-End Executive Manager', 100000, 2),
+  ('Front-End Project Manager', 80000, 3),
+  ('Back-End Project Manager', 90000, 3),
+  ('Marketing Project Manager', 90000, 3),
+  ('Chief Project Manager', 90000, 3),
+  ('Social Media Marketer', 80000, 4),
+  ('SEO Specialist', 80000, 4),
+  ('Market Research Analyst', 80000, 4),
+  ('Executive Marketing Manager', 100000, 4),
+  ('HR Specialist', 75000, 5),
+  ('Talent Manager', 80000, 5),
+  ('HR Compliance', 80000, 5),
+  ('HR Manager', 100000, 5);
