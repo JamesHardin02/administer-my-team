abd8080b0f3e45f63110d1a886a650875816a28c
JamesH <jwhusc7@gmail.com>
2022-05-11 19:02:40 -0400

    added employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,13 +8,9 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-  ('Visual Designer', 75000, 1),
-  ('UX/UI Designer', 80000, 1),
   ('Front-End Developer', 80000, 1),
   ('Front-End Executive Manager', 100000, 1),
   ('Back-End Developer', 85000, 2),
-  ('Database Developer', 90000, 2),
-  ('Database Administrator', 95000, 2),
   ('Back-End Executive Manager', 100000, 2),
   ('Front-End Project Manager', 80000, 3),
   ('Back-End Project Manager', 90000, 3),
@@ -22,9 +18,28 @@ VALUES
   ('Chief Project Manager', 90000, 3),
   ('Social Media Marketer', 80000, 4),
   ('SEO Specialist', 80000, 4),
-  ('Market Research Analyst', 80000, 4),
   ('Executive Marketing Manager', 100000, 4),
   ('HR Specialist', 75000, 5),
-  ('Talent Manager', 80000, 5),
-  ('HR Compliance', 80000, 5),
   ('HR Manager', 100000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Peter', 'Greenaway', 2, 1),
+  ('James', 'Fraser', 1, 1),
+  ('Jack', 'London', 1, 1),
+  ('Robert', 'Bruce', 1, 1),
+  ('Sandy', 'Powell', 4, 5),
+  ('Derek', 'Jarman', 3, 5),
+  ('Paolo', 'Pasolini', 3, 5),
+  ('Heathcote', 'Williams', 3, 5),
+  ('Samuel', 'Delany', 8, 9),
+  ('Emil', 'Zola', 5, 9),
+  ('Sissy', 'Coalpits', 6, 9),
+  ('Antoinette', 'Capet', 7, 9),
+  ('John', 'Dryden', 11, 13),
+  ('Tony', 'Duvert', 9, 13),
+  ('Dennis', 'Cooper', 9, 13),
+  ('Monica', 'Bellucci', 10, 13),
+  ('Aubrey', 'Beardsley', 13, 17),
+  ('Alexander', 'Pope', 12, 17),
+  ('Lionel', 'Johnson', 12, 17);
